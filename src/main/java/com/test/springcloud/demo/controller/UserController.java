93ed971689aa100f3b617bbdc9d81c1973ff0801
LiuZhen <1204736871@qq.com>
2020-04-17 16:38:59 +0800

    编写UserController

---
+++ src/main/java/com/test/springcloud/demo/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.test.springcloud.demo.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.test.springcloud.demo.entity.User;
+import com.test.springcloud.demo.entity.UserDto;
+import com.test.springcloud.demo.service.UserService;
+import com.test.springcloud.demo.util.response.AjaxResponseBody;
+import com.test.springcloud.demo.util.response.Result;
+import com.test.springcloud.demo.util.response.ResultStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AjaxResponseBody
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    @SuppressWarnings("unchecked")
+    public Result<List<UserDto>> findAll(Integer index){
+        Page<User> users = userService.getPage(index);
+        if(users != null){
+            return Result.success(users.getRecords().stream().map(UserDto::new).collect(Collectors.toList()));
+        }
+        return Result.success(Collections.EMPTY_LIST);
+    }
+
+    @GetMapping("/{id}")
+    public Result<UserDto> detail(@PathVariable Long id){
+        User user = userService.getUserInfo(id);
+        return (null != user)? Result.success(new UserDto(user)) : Result.failure(ResultStatus.WRONG_PARAMETERS);
+    }
+
+    @PostMapping("/{id}")
+    public Result<UserDto> update(@PathVariable Long id,
+                                  @RequestBody UserDto userDto){
+        userDto.setId(id);
+        User user = userService.save(userDto);
+        return (null != user)? Result.success(new UserDto(user)) : Result.failure(ResultStatus.WRONG_PARAMETERS);
+    }
+
+    @DeleteMapping("/{id}")
+    public Result<Boolean> delete(@PathVariable Long id){
+        userService.delete(id);
+        return Result.success(true);
+    }
+    
+}
